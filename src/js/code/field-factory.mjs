6823c6ad032a9bcc92d13aaa14277b52c58dcbc4
Podexus23 <santon23@gmail.com>
2022-03-31 01:08:01 +0300

    build field, put ships on it, some test

--- src/js/code/field-factory.mjs
+++ src/js/code/field-factory.mjs
@@ -1,12 +1,115 @@
 export default class Field {
+  static wave = `~`;
+
   constructor() {
-    const wave = `~`;
     this.board = new Array(10);
-    this.board.fill(wave);
-    this.board = this.board.map(() => this.board);
+    this.board.fill(Field.wave);
+    this.board = this.board.map(() => [...this.board]);
   }
 
   showField() {
     return this.board;
+  }
+
+  setShipOnField(ship, coordinates, direction = 'hor') {
+    const shipSize = ship.length;
+
+    const checkCoords = this.isCoordsValid(coordinates);
+    if (!checkCoords) throw new Error(`Sorry those coordinates doesn't exist`);
+
+    const place = this.isEmpty(coordinates, direction, shipSize);
+    if (!place) throw new Error(`Sorry thats impossible`);
+
+    if (direction === 'hor') {
+      this.setHorizontally(coordinates, shipSize);
+      this.setShipsBorder();
+    }
+    if (direction === 'vert') {
+      this.setVertically(coordinates, shipSize);
+      this.setShipsBorder();
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  isCoordsValid(coordinates) {
+    if (Math.min(...coordinates) < 0) return false;
+    if (Math.min(...coordinates) > 10) return false;
+    return true;
+  }
+
+  isEmpty([x, y], dir, size) {
+    if (dir === 'hor') {
+      for (let i = y; i < y + size; i += 1) {
+        if (i > 10) return false;
+        if (this.board[x][i] !== Field.wave) return false;
+      }
+    }
+    if (dir === 'vert') {
+      for (let i = x; i < x + size; i += 1) {
+        if (i > 10) return false;
+        if (this.board[i][y] !== Field.wave) return false;
+      }
+    }
+    return true;
+  }
+
+  setHorizontally(coordinates, size) {
+    const [x, y] = coordinates;
+    for (let i = y; i < y + size; i += 1) {
+      this.board[x][i] = '1';
+    }
+  }
+
+  setVertically(coordinates, size) {
+    const [x, y] = coordinates;
+    for (let i = x; i < x + size; i += 1) {
+      this.board[i][y] = '1';
+    }
+  }
+
+  setShipsBorder() {
+    const shippedSea = this.board;
+    shippedSea.forEach((wave, index) => {
+      if (wave.includes('1')) {
+        wave.forEach((cub, met) => {
+          if (cub === '1') {
+            if (
+              index - 1 >= 0 &&
+              met - 1 >= 0 &&
+              index + 1 <= 10 &&
+              met + 1 <= 10
+            ) {
+              // put border on top
+              (() => {
+                if (shippedSea[index - 1][met - 1] === '~')
+                  shippedSea[index - 1][met - 1] = 'b';
+                if (shippedSea[index - 1][met + 1] === '~')
+                  shippedSea[index - 1][met + 1] = 'b';
+
+                if (shippedSea[index - 1][met] === '~')
+                  shippedSea[index - 1][met] = 'b';
+              })();
+              // put border in the middle
+              (() => {
+                if (shippedSea[index][met - 1] === '~')
+                  shippedSea[index][met - 1] = 'b';
+
+                if (shippedSea[index][met + 1] === '~')
+                  shippedSea[index][met + 1] = 'b';
+              })();
+              // put border down
+              (() => {
+                if (shippedSea[index + 1][met - 1] === '~')
+                  shippedSea[index + 1][met - 1] = 'b';
+                if (shippedSea[index + 1][met + 1] === '~')
+                  shippedSea[index + 1][met + 1] = 'b';
+                if (shippedSea[index + 1][met] === '~')
+                  shippedSea[index + 1][met] = 'b';
+              })();
+            }
+          }
+        });
+      }
+    });
   }
 }
--- src/js/test/field-factory.test.mjs
+++ src/js/test/field-factory.test.mjs
@@ -1,10 +1,100 @@
 import Field from '../code/field-factory.mjs';
+import Ship from '../code/ship-factory.mjs';
 
-let someField = new Array(10);
-someField.fill('~');
-someField = someField.map((elem) => someField);
-
-it('I wanna see a sea', () => {
+test('I wanna see a sea', () => {
   const sea = new Field();
-  expect(sea.showField()).toEqual(someField);
+  const baza = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+  ];
+  expect(sea.showField()).toStrictEqual(baza);
 });
+
+test('place a little ship with 2 cell horizontally', () => {
+  const sea = new Field();
+  const firstClassShip = new Ship(2);
+  const coordinates = [2, 3];
+  const baza = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', 'b', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', '1', 'b', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', 'b', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+  ];
+  sea.setShipOnField(firstClassShip, coordinates);
+  expect(sea.showField()).toStrictEqual(baza);
+});
+
+test('place a little ship with 3 cell vertically', () => {
+  const sea = new Field();
+  const firstClassShip = new Ship(3);
+  // const secondClassShip = new Ship(4);
+  const coordinates = [2, 3];
+  // const coordinates2 = [5, 4];
+  const baza = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+  ];
+  sea.setShipOnField(firstClassShip, coordinates, 'vert');
+  // sea.setShipOnField(secondClassShip, coordinates2, 'vert');
+  expect(sea.showField()).toStrictEqual(baza);
+});
+test('encounter between two ships, must return Error', () => {
+  const sea = new Field();
+  const firstClassShip = new Ship(3);
+  const secondClassShip = new Ship(4);
+  const coordinates = [2, 3];
+  const coordinates2 = [5, 4];
+
+  sea.setShipOnField(firstClassShip, coordinates, 'vert');
+
+  expect(() =>
+    sea.setShipOnField(secondClassShip, coordinates2, 'hor')
+  ).toThrow();
+});
+test('encounter between two ships, without error', () => {
+  const sea = new Field();
+  const firstClassShip = new Ship(3);
+  const secondClassShip = new Ship(4);
+  const coordinates = [2, 3];
+  const coordinates2 = [7, 3];
+  const baza = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', '1', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', 'b', 'b', 'b', '~', '~'],
+    ['~', '~', 'b', '1', '1', '1', '1', 'b', '~', '~'],
+    ['~', '~', 'b', 'b', 'b', 'b', 'b', 'b', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+  ];
+  sea.setShipOnField(firstClassShip, coordinates, 'vert');
+  sea.setShipOnField(secondClassShip, coordinates2, 'hor');
+  expect(sea.showField()).toStrictEqual(baza);
+});
+
+// пофиксить координаты что бы они были [1, 10];
+// пофиксить ввод барьеров на границах полей, т.к. сейчас они вообще не выводятся
