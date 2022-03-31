b18f956054151a9ee0e15d2b57cadfcd766c4585
Podexus23 <santon23@gmail.com>
2022-04-01 00:25:40 +0300

    feat: add some rules for players

--- src/js/code/player.mjs
+++ src/js/code/player.mjs
@@ -1,5 +1,38 @@
 export default class Player {
-  constructor(name) {
+  constructor(name, player) {
     this.name = name;
+    this.player = player;
+    if (this.player !== 'human') {
+      this.makeCoords();
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  makeAMove(gameboard, coords) {
+    if (this.player === 'human') gameboard.receiveAttack(coords);
+    else gameboard.receiveAttack(...this.takeRandomCoords());
+  }
+
+  takeRandomCoords() {
+    const min = 0;
+    const max = this.botCoords.length;
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    const thatAShot = this.botCoords.slice(randomNumber, randomNumber + 1);
+    this.botCoords = [
+      ...this.botCoords.slice(0, randomNumber),
+      ...this.botCoords.slice(randomNumber + 1),
+    ];
+    console.log('shot:', thatAShot);
+    return thatAShot;
+  }
+
+  makeCoords() {
+    const row = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.botCoords = row
+      .map((elem) => {
+        const newRow = row.map((num) => [elem, num]);
+        return newRow;
+      })
+      .flat();
   }
 }
--- src/js/test/player.test.mjs
+++ src/js/test/player.test.mjs
@@ -1,3 +1,63 @@
 import Field from '../code/field-factory.mjs';
 import Ship from '../code/ship-factory.mjs';
 import Player from '../code/player.mjs';
+
+test('testin name Robocop', () => {
+  const bot = new Player('Robocop');
+  expect(bot.name).toBe('Robocop');
+});
+
+test('robocop make a shot at 2:2 and 1:1', () => {
+  const sea = new Field();
+  const lilShip = new Ship(2);
+  const coordinates = [2, 2];
+  const coordinates2 = [1, 1];
+  sea.setShipOnField(lilShip, coordinates);
+  const bot = new Player('Robocop', 'human');
+  bot.makeAMove(sea, coordinates);
+  bot.makeAMove(sea, coordinates2);
+  const baza = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', 'bh', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', 'b', '1sh', '1s', 'b', '~', '~', '~', '~', '~'],
+    ['~', 'b', 'b', 'b', 'b', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+  ];
+  expect(sea.showField()).toStrictEqual(baza);
+});
+test('robocop never shots twice in one place', () => {
+  const sea = new Field();
+  const lilShip = new Ship(2);
+  const coordinates = [2, 2];
+  sea.setShipOnField(lilShip, coordinates);
+  const bot = new Player('Robocop', 'loh');
+  bot.makeAMove(sea, coordinates);
+  bot.makeAMove(sea, coordinates);
+
+  expect(bot.botCoords.length).toStrictEqual(98);
+});
+test('robocop shoot everything', () => {
+  const sea = new Field();
+  const bot = new Player('Robocop', 'loh');
+  for (let i = 0; i < 100; i += 1) {
+    bot.makeAMove(sea);
+  }
+  const baza = [
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+    ['~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h', '~h'],
+  ];
+  expect(sea.showField()).toStrictEqual(baza);
+});
